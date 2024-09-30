802df24fbb0601fd9c7f018cbcd2b44413c5cf05
furkanavc1 <furkanavci039@gmail.com>
2024-09-30 13:57:31 +0300

    css file, prettier for tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,4 @@
+<script setup lang="ts"></script>
 <template>
-  <div class="text-yellow-500">asdasd</div>
-  <div class="text-red-500">asdasd</div>
-  <div class="text-blue-500">asdasd</div>
+  <div>asdasd</div>
 </template>
