125f09539c8428c9977a187578c0c68d08ad9bf5
furkanavc1 <furkanavci039@gmail.com>
2024-10-02 20:46:56 +0300

    delete uncessary code

--- components/PostItem.vue
+++ components/PostItem.vue
@@ -13,7 +13,6 @@ const { post } = defineProps<Props>();
       <div class="flex items-center space-x-2">
         <img
           src="https://pbs.twimg.com/profile_images/994592419705274369/RLplF55e_normal.jpg"
-          crossorigin="anonymous"
           alt="User Avatar"
           class="w-8 h-8 rounded-full"
         />
